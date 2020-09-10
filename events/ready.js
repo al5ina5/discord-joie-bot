fd9893ffc4018d897a25042e98c79740ca9a4864
Sebastian Alsina <alsinas@me.com>
2020-09-10 15:50:48 -0400

    Added status to bot

--- events/ready.js
+++ events/ready.js
@@ -2,4 +2,6 @@ module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}!`)
     console.log(`Commands loaded: ${process.env.BOT_PREFIX}${Object.keys(client.commands).join(`, ${process.env.BOT_PREFIX}`)}`)
     console.log(`Events loaded: ${Object.keys(client.events).join(`, `)}`)
+
+    client.user.setActivity('+help', { type: 'WATCHING' })
 }
